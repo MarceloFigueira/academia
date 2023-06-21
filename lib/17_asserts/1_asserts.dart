9539cc48fc449fd890c9460988ecbd1bb4ce9a52
Marcelo Figueira <marcelofigueira87@gmail.com>
2023-06-20 21:26:36 -0300

    aprendizado sobre asserts em dart

---
+++ lib/17_asserts/1_asserts.dart
@@ -0,0 +1,12 @@
+import 'package:academia/17_asserts/cliente.dart';
+
+void main() {
+  var cli = Cliente(
+    cpf: '12341234',
+    nome: 'marcelo figueira',
+    // razaoSocial: 'distribuidora',
+    // cnpj: '1232123',
+  );
+
+  print(cli.nome);
+}
---
+++ lib/17_asserts/cliente.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Cliente {
+  String? nome;
+  String? cpf;
+
+  String? razaoSocial;
+  String? cnpj;
+
+  Cliente({
+    this.nome,
+    this.cpf,
+    this.razaoSocial,
+    this.cnpj,
+  }) : assert(
+            (cpf != null)
+                ? nome != null && razaoSocial == null && cnpj == null
+                : true,
+            'voce enviou cpf junto com cnpj');
+}
